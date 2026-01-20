c11f9ddda98ac2beae4c3b34a09b55569c4927f7
Vidyesh Churi <vidyesh95@gmail.com>
2026-01-20 19:25:35 +0530

    feat: health check route added

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,12 +1,15 @@
 mod hello_world;
 mod project_version;
+mod health_check;
 
 use axum::{Router, routing::get};
 use crate::routes::hello_world::hello_world;
 use crate::routes::project_version::project_version;
+use crate::routes::health_check::health_check;
 
 pub fn create_routes() -> Router {
     Router::new()
-    .route("/", get(hello_world))
-    .route("/version", get(project_version))
+        .route("/", get(hello_world))
+        .route("/version", get(project_version))
+        .route("/health", get(health_check))
 }
